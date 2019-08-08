f73c2ec1055f08e9b318cef3a996b408eb7dc92a
ATTK <ATTK@DK01WV2250>
2019-08-08 16:54:07 +0200

    Second Lab is ready

---
+++ MobilePhone.UnitTestProject/FakeOutput.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MobilePhone.Base.GeneralInterfaces;
+
+namespace MobilePhone.UnitTestProject
+{
+    public class FakeOutput: IOutput
+    {
+        private string output;
+        public string Output{get { return output; }}
+
+
+        public void Write(string text)
+        {
+            output = output + text;
+        }
+
+        public void WriteLine(string text)
+        {
+            output = output +"\n"+ text;
+        }
+
+        public void Clean()
+        {
+            output = "";
+        }
+    }
+}
---
+++ MobilePhone.UnitTestProject/MobilePhoneUnitTests.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MobilePhone.Base.Components.Chargers;
+using MobilePhone.Base.Components.HeadSets;
+using MobilePhone.Base.MobilePhones;
+
+namespace MobilePhone.UnitTestProject
+{
+    [TestClass]
+    public class MobilePhoneUnitTests
+    {
+        private readonly MobilePhoneBase vNokia1100 = new Nokia1100(777);
+        private readonly FakeOutput vFakeOutput = new FakeOutput();
+        
+        #region HeadsetTests
+        [TestMethod]
+        public void iPhoneHeadsetTestMethod()
+        {
+            //arrange
+            vNokia1100.PlaybackComponent = HeadsetFactory.GetHeadset(Headsets.iPhoneHeadset, vFakeOutput);
+            string controlValue = "\niPhoneHeadset playback selected"+
+                                    "\nSet playback to mobile..." +
+                                    "\nPlay sound in mobile" +
+                                    "\niPhoneHeadset sound: Hi";
+            //act
+            vNokia1100.Play("Hi");
+            //assert
+            Assert.AreEqual(controlValue, vFakeOutput.Output);
+        }
+        [TestMethod]
+        public void SamsungHeadsetTestMethod()
+        {
+            //arrange
+            vNokia1100.PlaybackComponent = HeadsetFactory.GetHeadset(Headsets.SamsungHeadset, vFakeOutput);
+            string controlValue = "\nSamsungHeadset playback selected" +
+                                    "\nSet playback to mobile..." +
+                                    "\nPlay sound in mobile" +
+                                    "\nSamsungHeadset sound: Hi";
+            //act
+            vNokia1100.Play("Hi");
+            //assert
+            Assert.AreEqual(controlValue, vFakeOutput.Output);
+        }
+        [TestMethod]
+        public void UnofficialiPhoneHeadsetTestMethod()
+        {
+            //arrange
+            vNokia1100.PlaybackComponent = HeadsetFactory.GetHeadset(Headsets.UnofficialiPhoneHeadset, vFakeOutput);
+            string controlValue = "\nUnofficialiPhoneHeadset playback selected" +
+                                    "\nSet playback to mobile..." +
+                                    "\nPlay sound in mobile" +
+                                    "\nUnofficialiPhoneHeadset sound: Hi";
+            //act
+            vNokia1100.Play("Hi");
+            //assert
+            Assert.AreEqual(controlValue, vFakeOutput.Output);
+        }
+        [TestMethod]
+        public void PhoneSpeakerTestMethod()
+        {
+            //arrange
+            vNokia1100.PlaybackComponent = HeadsetFactory.GetHeadset(Headsets.PhoneSpeaker, vFakeOutput);
+            string controlValue = "\nPhoneSpeaker playback selected" +
+                                    "\nSet playback to mobile..." +
+                                    "\nPlay sound in mobile" +
+                                    "\nPhoneSpeaker sound: Hi";
+            //act
+            vNokia1100.Play("Hi");
+            //assert
+            Assert.AreEqual(controlValue, vFakeOutput.Output);
+        }
+        #endregion
+        #region ChargerTests
+        [TestMethod]
+        public void FastChargeTestMethod()
+        {
+            //arrange
+            vFakeOutput.Clean();
+            vNokia1100.ChargerComponent = ChargeFactory.GetCharge(Charges.FastCharge, vFakeOutput);
+            string controlValue = "\nFastCharge charging fast";
+            //act
+            vNokia1100.Charge();
+            //assert
+            Assert.AreEqual(controlValue, vFakeOutput.Output);
+        }
+        [TestMethod]
+        public void UsualChargeTestMethod()
+        {
+            //arrange
+            vFakeOutput.Clean();
+            vNokia1100.ChargerComponent = ChargeFactory.GetCharge(Charges.UsualCharge, vFakeOutput);
+            string controlValue = "\nUsualCharge charging slowly";
+            //act
+            vNokia1100.Charge();
+            //assert
+            Assert.AreEqual(controlValue, vFakeOutput.Output);
+        }
+        #endregion
+    }
+}
